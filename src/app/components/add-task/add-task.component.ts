def74da3299a2a74cc094a92e05fca2015af1381
WebHappy <WebHappy@happy.com>
2023-02-11 12:27:18 +0100

    complete init

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-add-task',
@@ -12,6 +13,12 @@ export class AddTaskComponent {
   text: string = "";
   day: string = "";
   reminder: boolean = false;
+  showAddTask?: boolean;
+  subscription?: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   onSubmt() {
     if (!this.text) {
